eaa60157ec3db989a6838bc651b14de0da95abcf
danieloprado <danieloprado@outlook.com>
2015-08-05 17:05:09 -0300

    controller

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-  angular.module("gorilla.app", []);
+  angular.module("gorilla.app", [])
+
+  .controller("profileController", ["$scope", function($scope) {
+    $scope.profile = {
+      name: "",
+      male: false
+    };
+    $scope.profiles = [{"name":"daniel prado","male":true},{"name":"maria garcia","male":false}];
+
+    $scope.$watch("profile.name", function(value) {
+      $scope.message = value ? "com conteudo" : "escreva um nome";
+    });
+
+    $scope.$watch("profile", function(value) {
+      $scope.messageProfile = new Date();
+    }, true);
+
+    $scope.add = function() {
+      if ($scope.profile.name.length == 0) {
+        alert("precisa de um nome!");
+        return;
+      }
+
+      var copy = angular.copy($scope.profile);
+      $scope.profiles.push(copy);
+
+      $scope.profile = {
+        name: "",
+        male: false
+      };
+    }
+
+    $scope.remove = function(index) {
+      $scope.profiles.splice(index, 1);
+    }
+  }]);
