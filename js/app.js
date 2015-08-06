7b957914c409ea4c2c6978d6f0db3d6039ded0ed
danieloprado <danieloprado@outlook.com>
2015-08-06 09:53:43 -0300

    watchCollection, favicon and layout changes

--- js/app.js
+++ js/app.js
@@ -1,36 +1,47 @@
-  angular.module("gorilla.app", [])
+angular.module("gorilla.app", [])
 
-  .controller("profileController", ["$scope", function($scope) {
+.controller("profileController", ["$scope", function($scope) {
+  $scope.profile = {
+    name: "",
+      male: false
+  };
+
+  $scope.profiles = [{
+    "name": "daniel prado",
+    "male": true
+  }, {
+    "name": "maria garcia",
+    "male": false
+  }];
+
+  $scope.$watch("profile.name", function(value) {
+    $scope.message = value ? "com conteudo" : "escreva um nome";
+  });
+
+  $scope.$watch("profile", function(value) {
+    $scope.messageProfile = new Date();
+  }, true);
+
+  $scope.$watchCollection("profiles", function() {
+    $scope.lastUpdate = new Date();
+  }, true);
+
+  $scope.add = function() {
+    if ($scope.profile.name.length == 0) {
+      alert("precisa de um nome!");
+      return;
+    }
+
+    var copy = angular.copy($scope.profile);
+    $scope.profiles.push(copy);
+
     $scope.profile = {
       name: "",
       male: false
     };
-    $scope.profiles = [{"name":"daniel prado","male":true},{"name":"maria garcia","male":false}];
+  }
 
-    $scope.$watch("profile.name", function(value) {
-      $scope.message = value ? "com conteudo" : "escreva um nome";
-    });
-
-    $scope.$watch("profile", function(value) {
-      $scope.messageProfile = new Date();
-    }, true);
-
-    $scope.add = function() {
-      if ($scope.profile.name.length == 0) {
-        alert("precisa de um nome!");
-        return;
-      }
-
-      var copy = angular.copy($scope.profile);
-      $scope.profiles.push(copy);
-
-      $scope.profile = {
-        name: "",
-        male: false
-      };
-    }
-
-    $scope.remove = function(index) {
-      $scope.profiles.splice(index, 1);
-    }
-  }]);
+  $scope.remove = function(index) {
+    $scope.profiles.splice(index, 1);
+  }
+}]);
